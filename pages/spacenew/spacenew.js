763b739c904dcf39a209fbe36db3ec03b55cdb38
frank zhu <qf.zhu.frank@gmail.com>
2019-04-05 17:23:46 +0800

    ready

--- pages/spacenew/spacenew.js
+++ pages/spacenew/spacenew.js
@@ -1,51 +1,30 @@
+// pages/spacenew/spacenew.js
 var app = getApp()
+// const districts = ["Huangpu", "Xuhui", "Changning", "Jing'an", "Putuo", "Hongkou", "Yangpu", "Minhang", "Baoshan", "Jiading", "Pudong", "Jinshan", "Songjiang", "Qingpu", "Fengxian", "Chongming"]
+
 
 Page({
+
+  /**
+   * Page initial data
+   */
   data: {
-    selectShow: false,
-    selectData: ["District", "Huangpu", "Xuhui", "Changning", "Jing'an", "Putuo", "Hongkou", "Yangpu", "Minhang", "Baoshan", "Jiading", "Pudong", "Jinshan", "Songjiang", "Qingpu", "Fengxian", "Chongming"],
-    index: 0,
+    districts: app.globalData.districts
   },
 
-  selectTap() {
-    this.setData({
-      selectShow: !this.data.selectShow
-    });
+  // bindChange: function (e) {
+  //   const val = e.detail.value
+  //   console.log(val)
+  //   this.setData({
+  //     district: this.data.districts[val[0]],
+  //   })
+  // },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
   },
-
-  optionTap(e) {
-    let Index = e.currentTarget.dataset.index;
-    this.setData({
-      index: Index,
-      selectShow: !this.data.selectShow
-    });
-  },
-  // })
-  // const districts = ["Huangpu", "Xuhui", "Changning", "Jing'an", "Putuo", "Hongkou", "Yangpu", "Minhang", "Baoshan", "Jiading", "Pudong", "Jinshan", "Songjiang", "Qingpu", "Fengxian", "Chongming"]
-
-
-  // Page({
-
-  //   /**
-  //    * Page initial data
-  //    */
-  //   data: {
-  //     districts: districts
-  //   },
-
-  //   // bindChange: function (e) {
-  //   //   const val = e.detail.value
-  //   //   console.log(val)
-  //   //   this.setData({
-  //   //     district: this.data.districts[val[0]],
-  //   //   })
-  //   // },
-
-  //   /**
-  //    * Lifecycle function--Called when page load
-  //    */
-  //   onLoad: function (options) {
-  //   },
 
   // New Space Submission
   bindSubmit: function (s) {
@@ -76,6 +55,7 @@ Page({
     // Get api data
     wx.request({
       url: `https://spaceshare-frank657.herokuapp.com/api/v1/spaces`,
+      // url: `http://localhost:3000/api/v1/spaces`,
       method: 'POST',
       data: space,
       success(a) {
@@ -89,6 +69,8 @@ Page({
     });
 
   },
+
+
 
 
 
@@ -142,3 +124,132 @@ Page({
 
   }
 })
+
+
+
+// // pages/spacenew/spacenew.js
+// var app = getApp()
+// // const districts = ["Huangpu", "Xuhui", "Changning", "Jing'an", "Putuo", "Hongkou", "Yangpu", "Minhang", "Baoshan", "Jiading", "Pudong", "Jinshan", "Songjiang", "Qingpu", "Fengxian", "Chongming"]
+
+
+// Page({
+
+//   /**
+//    * Page initial data
+//    */
+//   data: {
+//     districts: app.globalData.districts
+//   },
+
+//   // bindChange: function (e) {
+//   //   const val = e.detail.value
+//   //   console.log(val)
+//   //   this.setData({
+//   //     district: this.data.districts[val[0]],
+//   //   })
+//   // },
+
+//   /**
+//    * Lifecycle function--Called when page load
+//    */
+//   onLoad: function (options) {
+//   },
+ 
+//   // New Space Submission
+//   bindSubmit: function (s) {
+//     const name = s.detail.value.name;
+//     const district = s.detail.value.district;
+//     const address = s.detail.value.address_details;
+//     const spots = s.detail.value.available_spots;
+//     const picture = s.detail.value.picture;
+//     const price = s.detail.value.price
+//     const amenities = s.detail.value.amenities
+
+//     const space = {
+//       name: name,
+//       district: district, 
+//       address_details: address,
+//       available_spots: spots,
+//       picture: 'https://source.unsplash.com/800x450/?office',
+//       price: price,
+//       user_id: app.globalData.userId,
+//       wifi: amenities.includes('wifi'),
+//       sofa_area: amenities.includes('sofa'),
+//       purified_air: amenities.includes('air'),
+//       coffee: amenities.includes('coffee'),
+//       beer: amenities.includes('beer')
+//     }
+//     console.log(space)
+
+//     // Get api data
+//     wx.request({
+//       url: `https://spaceshare-frank657.herokuapp.com/api/v1/spaces`,
+//       // url: `http://localhost:3000/api/v1/spaces`,
+//       method: 'POST',
+//       data: space,
+//       success(a) {
+//         console.log(11,a)
+//         // set data
+//         // wx.switchTab({
+//         wx.reLaunch({
+//           url: '/pages/me/me'
+//         });
+//       }
+//     });
+
+//   },
+
+
+
+
+
+
+//   /**
+//    * Lifecycle function--Called when page is initially rendered
+//    */
+//   onReady: function () {
+
+//   },
+
+//   /**
+//    * Lifecycle function--Called when page show
+//    */
+//   onShow: function () {
+
+//   },
+
+//   /**
+//    * Lifecycle function--Called when page hide
+//    */
+//   onHide: function () {
+
+//   },
+
+//   /**
+//    * Lifecycle function--Called when page unload
+//    */
+//   onUnload: function () {
+
+//   },
+
+//   /**
+//    * Page event handler function--Called when user drop down
+//    */
+//   onPullDownRefresh: function () {
+
+//   },
+
+//   /**
+//    * Called when page reach bottom
+//    */
+//   onReachBottom: function () {
+
+//   },
+
+//   /**
+//    * Called when user click on the top right corner to share
+//    */
+//   onShareAppMessage: function () {
+
+//   }
+// })
